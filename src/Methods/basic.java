9053b6d69ca67e130ff19f9cbb0d6511a45c4849
sadanand-tech <sadanand241299@gmail.com>
2025-02-19 18:59:03 +0530

    method part 1

---
+++ src/Methods/basic.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class basic {
+    public static void main(String[] args) {
+        add();
+    }
+    public static void add(){
+        int a=2,b=3;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
