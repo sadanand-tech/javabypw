f14fecb7105fb0cf27f66f7e7690f27fad63751b
sadanand-tech <sadanand241299@gmail.com>
2025-02-22 22:48:20 +0530

    method2

---
+++ src/Methods/basic2.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class basic2 {
+    public static void main(String[] args) {
+        add();
+    }
+    public static void add(){
+        int a=2,b=3;
+        int c=a+b;
+        System.out.println(c);
+    }
+
+}
