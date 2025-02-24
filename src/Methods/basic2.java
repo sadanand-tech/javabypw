2d8ac65e0e154054d44d4f40f0638ac31d082408
sadanand-tech <sadanand241299@gmail.com>
2025-02-24 21:56:08 +0530

    method 3

--- src/Methods/basic2.java
+++ src/Methods/basic2.java
@@ -4,6 +4,7 @@ public class basic2 {
     public static void main(String[] args) {
         add();
         sub();
+        multi();
     }
     public static void add(){
         int a=2,b=3;
@@ -11,6 +12,11 @@ public class basic2 {
         System.out.println(c);
     }
     public static void sub(){
+        int a=8,b=5;
+        int c=a-b;
+        System.out.println(c);
+    }
+    public static void multi(){
         int a=8,b=5;
         int c=a-b;
         System.out.println(c);
